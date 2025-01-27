e337b7dcbdeca95e045f974f32149c135e2f0b6a
charnelledev <kengnemaiva54@gmail.com>
2025-01-27 16:15:17 +0100

    lecon03-traitement de donne

--- cours/script.js
+++ cours/script.js
@@ -36,8 +36,89 @@ let person ={
     isnull:null
 }
 console.log(person.name);
+console.log(person.country[1]);
+
+
+let nom ='myra'
+console.log(nom.length);
+
+
 
 
 //--tableau -02
 
-let array3 = ["bafou",42,true,"bafang","bamenda",null]
+let array3 = ["bafou",42,true,"bafang","bamenda",null,{
+    name:"myra",
+    age:42,
+}];
+console.log(array3[6].age);
+console.log(array3[6].name);
+
+
+
+// affiche 42 
+
+// affiche bamenda ==
+
+
+//----les objets et plus
+
+let objet = {
+    firstname: "rosto",
+    lastname: "dev",
+    age: 29,
+    technos: ["Javascript", "NodeJs", "React", "laravel", "Flutter"],
+    admin: false
+    }
+
+    console.log(typeof(objet));
+    console.log(objet.technos[2]);
+
+
+
+
+
+
+
+    // Stocké les données
+let data = [
+    {
+      firstname: "rosto1",
+      lastname: "dev1",
+      age: 30,
+      technos: ["Javascript", "NodeJs", "React", "laravel", "Flutter"],
+      adress: "MarchéB",
+      admin: false
+    },
+
+    {
+      firstname: "rosto2",
+      lastname: "dev2",
+      age: 29,
+      technos: ["Php", "NodeJs", "React", "Python", "Flutter"],
+      admin: false
+    },
+
+    {
+      firstname: "rosto3",
+      lastname: "dev3",
+      age: 32,
+      technos: ["CSS", "NodeJs", "React", "Taillwindcss", "Flutter"],
+      admin: true
+    },
+  ];
+
+  console.log(data[2].technos);
+  
+
+  //les structures de controle
+
+  if(data[0].age>data[1].age){
+    console.log(data[0].firstname + " est plus age que "+ data[1].firstname);
+    
+  }else{
+    console.log(data[0].firstname + "est plus jeune que " + data[1].firstname);
+    
+  }
+    
+    
