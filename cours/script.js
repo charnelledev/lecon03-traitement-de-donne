3e94db41f92cb675769eba74dc9a7c3b8c266e45
charnelledev <kengnemaiva54@gmail.com>
2025-01-27 15:29:32 +0100

    lecon03-traitement de donne

--- cours/script.js
+++ cours/script.js
@@ -6,6 +6,38 @@ console.log(typeof(number));
 
 let boolean = true;
 let object_null = null;
-console.log(typeof(object_null));
-let maVariable;type underfined
+console.log(object_null);
 
+
+let array = [1,2,3,4,5];
+console.log(typeof(array));
+
+
+//let maVariable;//type underfined
+
+
+
+//les tableaux
+
+//-tableau -01--
+let array2 = ["bafou", "batie","bafang","bamenda"];
+
+console.log(array2 [0][2] );//f
+console.log(array2.length);//4
+
+
+//--objet
+let person ={
+    name:"bafou",
+    age:42,
+    istrue:true,
+    city:"baffang",
+    country:"cameroon",
+    isnull:null
+}
+console.log(person.name);
+
+
+//--tableau -02
+
+let array3 = ["bafou",42,true,"bafang","bamenda",null]
