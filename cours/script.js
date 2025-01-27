25f0534d4f02a3ffdc3479cb17e77a5951aa1912
charnelledev <kengnemaiva54@gmail.com>
2025-01-27 15:10:54 +0100

    lecon03-traitement de donne

---
+++ cours/script.js
@@ -0,0 +1,11 @@
+let string = "hello world";
+console.log(typeof(string));
+
+let number = 29;
+console.log(typeof(number));
+
+let boolean = true;
+let object_null = null;
+console.log(typeof(object_null));
+let maVariable;type underfined
+
