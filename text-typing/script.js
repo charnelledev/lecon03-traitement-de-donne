e902837aa902925f5094abbab75e28b1cc59b255
charnelledev <kengnemaiva54@gmail.com>
2025-01-28 11:37:14 +0100

    lecon03-traitement de donne

--- text-typing/script.js
+++ text-typing/script.js
@@ -5,3 +5,37 @@ let array = [
     "Graphiste",
     "Web design"
 ]
+
+let wordIndex = 0;
+let letterIndex = 0;
+// letter.textContent = array[2][2];
+ 
+ const createLetter = ()=>{
+    const letter = document.createElement('span');
+    target.appendChild(letter);
+     letter.textContent = array[wordIndex][letterIndex];
+     setTimeout(()=>{
+    letter.remove();
+     }, 2000);
+ }
+    const loop = () =>{
+        setTimeout(()=>{
+            if (letterIndex <array[wordIndex].length){
+               createLetter();
+               letterIndex++;
+               loop();
+            }else{
+                setTimeout(()=>{
+                wordIndex++;
+                letterIndex = 0;
+                loop();
+                // if (wordIndex<array.length){
+                //     loop();
+                // }
+                },2000);
+            }
+           },60);
+           }
+    loop()
+
+ 
